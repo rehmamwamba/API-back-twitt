aa1c85307b4cb6c999fbd1c00adc2a80a55daae7
rehmamwamba <147811270+rehmamwamba@users.noreply.github.com>
2024-02-23 15:43:46 +0200

    Add files via upload

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
-// db.js
+// config/database.js
 
-const { Pool } = require('pg');
+const mysql = require('mysql2');
 
-// Configuration de la connexion à la base de données PostgreSQL
-const pool = new Pool({
-    user: 'your-db-username', // Remplacez par le nom d'utilisateur de votre base de données
-    host: 'localhost',
-    database: 'your-db-name', // Remplacez par le nom de votre base de données
-    password: 'your-db-password', // Remplacez par le mot de passe de votre base de données
-    port: 5432,
+const connection = mysql.createConnection({
+  host: '"3300',
+  user: 'mwamba',
+  password: 'votre_mot_de_passe',
+  database: 'mysql2'
 });
 
-module.exports = pool;
+module.exports = connection;
